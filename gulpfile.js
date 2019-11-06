3ebbebad469fe1bfbc05226032b189196a2d4aff
derpierre65 <git@derpierre65.de>
2019-11-06 23:24:31 +0100

    added gulp-strip-debug (remove console.* after build) and changed destination directory to remove gulp-copy

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,11 @@
-const gulp = require("gulp");
+const gulp = require('gulp');
+const webpack = require('webpack-stream');
+const stripDebug = require('gulp-strip-debug');
 
-const webpack = require("webpack-stream");
-
-const gulpCopy = require("gulp-copy");
-
-const path = require("path");
-
-gulp.task("build", function() {
+gulp.task('build', function () {
 	return gulp
-		.src("index.ts")
-		.pipe(webpack(require("./webpack.config")))
-		.pipe(gulp.dest("dist"))
-		.pipe(
-			gulpCopy(path.resolve(__dirname), {
-				prefix: 1
-			})
-		);
+		.src('index.ts')
+		.pipe(webpack(require('./webpack.config')))
+		.pipe(stripDebug())
+		.pipe(gulp.dest('./'));
 });
